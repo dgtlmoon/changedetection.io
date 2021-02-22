c59838a6e461f137d6c01f32c2697d26ac6f9b1e
Leigh Morresi <275001+dgtlmoon@users.noreply.github.com>
2021-02-22 08:38:41 +0100

    Issue #5 - Remove arbitrary '600' minutes limit

--- backend/__init__.py
+++ backend/__init__.py
@@ -197,14 +197,14 @@ def changedetection_app(config=None, datastore_o=None):
                 messages.append({'class': 'error', 'message': "Invalid value given, use an integer."})
 
             else:
-                if minutes >= 5 and minutes <= 600:
+                if minutes >= 5:
                     datastore.data['settings']['requests']['minutes_between_check'] = minutes
                     datastore.needs_write = True
 
                     messages.append({'class': 'ok', 'message': "Updated"})
                 else:
                     messages.append(
-                        {'class': 'error', 'message': "Must be equal to or greater than 5 and less than 600 minutes"})
+                        {'class': 'error', 'message': "Must be atleast 5 minutes."})
 
         output = render_template("settings.html", messages=messages,
                                  minutes=datastore.data['settings']['requests']['minutes_between_check'])
