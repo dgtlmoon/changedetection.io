4953e253e94907de5e0bf3ea74379578663e3300
dgtlmoon <dgtlmoon@gmail.com>
2021-04-30 17:17:23 +1000

    bump to 0.29

--- backend/store.py
+++ backend/store.py
@@ -110,7 +110,7 @@ class ChangeDetectionStore:
                 self.add_watch(url='https://changedetection.io', tag='Tech news')
 
 
-        self.__data['version_tag'] = "0.28"
+        self.__data['version_tag'] = "0.29"
 
         if not 'app_guid' in self.__data:
             self.__data['app_guid'] = str(uuid_builder.uuid4())
