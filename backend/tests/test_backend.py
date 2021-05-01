1d0018dced895dd59943fcd3b3b58ef73503452e
dgtlmoon <dgtlmoon@gmail.com>
2021-05-01 11:55:24 +1000

    - Relabel login button - misc test cleanup

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -129,51 +129,58 @@ def test_check_basic_change_detection_functionality(client, live_server):
     assert b'Deleted' in res.data
 
 
-def test_check_access_control(client):
+def test_check_access_control(app, client):
+    # Still doesnt work, but this is closer.
     return
-    # @note: does not seem to handle the last logout step correctly, we're still logged in.. but yet..
-    #        pytest team keep telling us that we have a new context.. i'm lost :(
+    with app.test_client() as c:
 
-    # Add our URL to the import page
-    res = client.post(
-        url_for("settings_page"),
-        data={"password": "foobar"},
-        follow_redirects=True
-    )
-    assert b"LOG OUT" not in res.data
+        # Check we dont have any password protection enabled yet.
+        res = c.get(url_for("settings_page"))
+        assert b"Remove password" not in res.data
 
-    client.get(url_for("import_page"), follow_redirects=True)
-    assert b"Password" in res.data
+        # Enable password check.
+        res = c.post(
+            url_for("settings_page"),
+            data={"password": "foobar"},
+            follow_redirects=True
+        )
+        assert b"Password protection enabled." in res.data
+        assert b"LOG OUT" not in res.data
+        print ("SESSION:", res.session)
+        # Check we hit the login
 
-    # Menu should not be available yet
-    assert b"SETTINGS" not in res.data
-    assert b"BACKUP" not in res.data
-    assert b"IMPORT" not in res.data
+        res = c.get(url_for("settings_page"), follow_redirects=True)
+        res = c.get(url_for("login"), follow_redirects=True)
 
+        assert b"Login" in res.data
 
-
-    #defaultuser@changedetection.io is actually hardcoded for now, we only use a single password
-    res = client.post(
-        url_for("login"),
-        data={"password": "foobar", "email": "defaultuser@changedetection.io"},
-        follow_redirects=True
-    )
-
-    assert b"LOG OUT" in res.data
-
-    client.get(url_for("settings_page"), follow_redirects=True)
-    # Menu should be available now
-    assert b"SETTINGS" in res.data
-    assert b"BACKUP" in res.data
-    assert b"IMPORT" in res.data
+        print ("DEBUG >>>>>",res.data)
+        # Menu should not be available yet
+        assert b"SETTINGS" not in res.data
+        assert b"BACKUP" not in res.data
+        assert b"IMPORT" not in res.data
 
 
-    assert b"LOG OUT" in res.data
 
-    # Now remove the password so other tests function, @todo this should happen before each test automatically
+        #defaultuser@changedetection.io is actually hardcoded for now, we only use a single password
+        res = c.post(
+            url_for("login"),
+            data={"password": "foobar", "email": "defaultuser@changedetection.io"},
+            follow_redirects=True
+        )
 
-    print(res.data)
-    client.get(url_for("settings_page", removepassword="true"), follow_redirects=True)
+        assert b"LOG OUT" in res.data
+        res = c.get(url_for("settings_page"))
 
-    client.get(url_for("import_page", removepassword="true"), follow_redirects=True)
-    assert b"LOG OUT" not in res.data
+        # Menu should be available now
+        assert b"SETTINGS" in res.data
+        assert b"BACKUP" in res.data
+        assert b"IMPORT" in res.data
+
+        assert b"LOG OUT" in res.data
+
+        # Now remove the password so other tests function, @todo this should happen before each test automatically
+
+        c.get(url_for("settings_page", removepassword="true"))
+        c.get(url_for("import_page"))
+        assert b"LOG OUT" not in res.data
