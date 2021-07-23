2b3c49a4f64c9a22c5d0179603ddc99554bd9d1b
dgtlmoon <dgtlmoon@gmail.com>
2021-07-23 20:15:29 +1000

    Tidy up function

--- backend/html_tools.py
+++ backend/html_tools.py
@@ -24,3 +24,25 @@ def extract_element(find='title', html_content=''):
 
     return element_text
 
+def extract_json_as_string(content, jsonpath_filter):
+    # POC hack, @todo rename vars, see how it fits in with the javascript version
+    import json
+    from jsonpath_ng import jsonpath, parse
+
+    json_data = json.loads(content)
+    jsonpath_expression = parse(jsonpath_filter.replace('json:', ''))
+    match = jsonpath_expression.find(json_data)
+    s = []
+
+    # More than one result, we will return it as a JSON list.
+    if len(match) > 1:
+        for i in match:
+            s.append(i.value)
+
+    # Single value, use just the value, as it could be later used in a token in notifications.
+    if len(match) == 1:
+        s = match[0].value
+
+    stripped_text_from_html = json.dumps(s, indent=4)
+
+    return stripped_text_from_html
