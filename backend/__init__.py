f8ea49e6f9ef4db464f40d5c49d720b9f41c495f
dgtlmoon <dgtlmoon@gmail.com>
2021-08-15 14:01:23 +0200

    set real path

--- backend/__init__.py
+++ backend/__init__.py
@@ -46,7 +46,7 @@ notification_q = queue.Queue()
 # Needs to be set this way because we also build and publish via pip
 app = Flask(__name__,
             static_url_path="{}/static".format(os.path.realpath(__file__)),
-            root_path=os.path.realpath(__file__))
+            template_folder="{}/templates".format(os.path.realpath(__file__)))
 
 # Stop browser caching of assets
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
