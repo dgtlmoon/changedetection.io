84dce3994100773409a3f8bd7aeb804968e4eb2c
dgtlmoon <dgtlmoon@gmail.com>
2022-03-22 22:56:39 +0100

    Use a smarter default dir for windows users

--- changedetectionio/changedetection.py
+++ changedetectionio/changedetection.py
@@ -16,9 +16,15 @@ def main():
     host = ''
     port = os.environ.get('PORT') or 5000
     do_cleanup = False
+    datastore_path = None
 
-    # Must be absolute so that send_from_directory doesnt try to make it relative to backend/
-    datastore_path = os.path.join(os.getcwd(), "../datastore")
+    # On Windows, create and use a default path.
+    if os.name == 'nt':
+        datastore_path = os.path.expandvars(r'%APPDATA%\changedetection.io')
+        os.makedirs(datastore_path, exist_ok=True)
+    else:
+        # Must be absolute so that send_from_directory doesnt try to make it relative to backend/
+        datastore_path = os.path.join(os.getcwd(), "../datastore")
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "Ccsd:h:p:", "port")
