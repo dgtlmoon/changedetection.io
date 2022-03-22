c12645363ba03c7f44d92e0fd127bd097cb786b1
dgtlmoon <dgtlmoon@gmail.com>
2022-03-22 21:10:11 +0100

    cleanup text

--- changedetection.py
+++ changedetection.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-# Entry-point for running from the CLI when not installed via PIP, Pip will handle the console_scripts entry_points's from setup.py
-# It's recommended to use `pip3 install changedetection.io` and start with `changedetection.py` instead, it sould be linkd to your global path.
+# Entry-point for running from the CLI when not installed via Pip, Pip will handle the console_scripts entry_points's from setup.py
+# It's recommended to use `pip3 install changedetection.io` and start with `changedetection.py` instead, it will be linkd to your global path.
+# or Docker.
+# Read more https://github.com/dgtlmoon/changedetection.io/wiki
 
 from changedetectionio import changedetection
 
