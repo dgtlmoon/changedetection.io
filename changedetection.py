82fe20e2ea361ee2d8fd4746bd8d7df90beaeb24
dgtlmoon <dgtlmoon@gmail.com>
2022-03-22 19:58:26 +0100

    fix wrapper text

--- changedetection.py
+++ changedetection.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-# Entry-point for running from the CLI when not installed via PIP
-# Pip will handle the console_scripts entry_points's from setup.py
+# Entry-point for running from the CLI when not installed via PIP, Pip will handle the console_scripts entry_points's from setup.py
+# It's recommended to use `pip3 install changedetection.io` and start with `changedetection.py` instead, it sould be linkd to your global path.
+
 from changedetectionio import changedetection
 
 if __name__ == '__main__':
