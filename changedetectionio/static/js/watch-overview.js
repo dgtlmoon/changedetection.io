4b7774db29edea65ecf6c3a4d06e4892238ae08a
dgtlmoon <dgtlmoon@gmail.com>
2022-04-26 09:56:06 +0200

    POC

--- changedetectionio/static/js/watch-overview.js
+++ changedetectionio/static/js/watch-overview.js
@@ -3,4 +3,20 @@ $(function () {
   $('.diff-link').click(function () {
     $(this).closest('.unviewed').removeClass('unviewed');
   });
+
+  // after page fetch, inject this JS
+  // build a map of all elements and their positions (maybe that only include text?)
+  var p = $( "*" );
+  for (var i = 0; i < p.length; i++) {
+     console.log($(p[i]).offset());
+     console.log($(p[i]).width());
+     console.log($(p[i]).height());
+     console.log(getXPath( p[i]));
+  }
+
+  // overlay it on a rendered image of the page
+
+
+
+  //p.html( "left: " + offset.left + ", top: " + offset.top );
 });
