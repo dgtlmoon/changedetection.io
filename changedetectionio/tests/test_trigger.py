01ea3a73e38820cdfd4768ce033a4d4a5266e37a
dgtlmoon <dgtlmoon@gmail.com>
2022-05-21 22:09:22 +0200

    Re #616 - adding extra test

--- changedetectionio/tests/test_trigger.py
+++ changedetectionio/tests/test_trigger.py
@@ -129,6 +129,11 @@ def test_trigger_functionality(client, live_server):
     time.sleep(sleep_time_for_fetch_thread)
     res = client.get(url_for("index"))
     assert b'unviewed' in res.data
+    
+    # https://github.com/dgtlmoon/changedetection.io/issues/616
+    # Apparently the actual snapshot that contains the trigger never shows
+    res = client.get(url_for("diff_history_page", uuid="first"))
+    assert b'foobar123' in res.data
 
     # Check the preview/highlighter, we should be able to see what we triggered on, but it should be highlighted
     res = client.get(url_for("preview_page", uuid="first"))
