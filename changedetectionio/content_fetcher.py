169c293143d7145f6f28f3d54b0ef70f14b2faf4
dgtlmoon <dgtlmoon@gmail.com>
2022-07-10 13:55:29 +0200

    Playwright - log console errors to output

--- changedetectionio/content_fetcher.py
+++ changedetectionio/content_fetcher.py
@@ -309,7 +309,10 @@ class base_html_playwright(Fetcher):
                 page.set_default_navigation_timeout(90000)
                 page.set_default_timeout(90000)
 
-               # Bug - never set viewport size BEFORE page.goto
+                # Listen for all console events and handle errors
+                page.on("console", lambda msg: print(f"Playwright console: Watch URL: {url} {msg.type}: {msg.text} {msg.args}"))
+
+                # Bug - never set viewport size BEFORE page.goto
 
                 # Waits for the next navigation. Using Python context manager
                 # prevents a race condition between clicking and waiting for a navigation.
