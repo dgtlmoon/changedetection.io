2ba3a6d53f928f8555d13de0d6da9642756f18e5
dgtlmoon <dgtlmoon@gmail.com>
2022-07-10 20:05:48 +0200

    Test improvement: Extract text should return all matches

--- changedetectionio/tests/test_extract_regex.py
+++ changedetectionio/tests/test_extract_regex.py
@@ -33,7 +33,7 @@ def set_modified_response():
      </br>
      So let's see what happens.  </br>
      <div id="sametext">Some text thats the same</div>
-     <div id="changetext">Some text that did change ( 1000 online <br/> 80 guests)</div>
+     <div id="changetext">Some text that did change ( 1000 online <br/> 80 guests<br/>  2000 online )</div>
      </body>
      </html>
     """
@@ -119,7 +119,10 @@ def test_check_filter_and_regex_extract(client, live_server):
 
     # Class will be blank for now because the frontend didnt apply the diff
     assert b'<div class="">1000 online' in res.data
-    
+
+    # All regex matching should be here
+    assert b'<div class="">2000 online' in res.data
+
     # Both regexs should be here
     assert b'<div class="">80 guests' in res.data
 
