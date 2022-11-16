b79c14a94b59414abc5559bd8e2e5803a5cff763
Matthias Bilger <matthias@bilger.info>
2022-11-16 19:30:36 +0100

    Update changedetectionio/tests/test_notification.py
    Co-authored-by: dgtlmoon <leigh@morresi.net>

--- changedetectionio/tests/test_notification.py
+++ changedetectionio/tests/test_notification.py
@@ -75,7 +75,7 @@ def test_check_notification(client, live_server):
     datastore = 'test-datastore/'
 
     for datastoreelement in glob(datastore + "*/"):
-        with open(datastoreelement + 'last-screenshot.png', 'wb+') as f:
+        with open(datastoreelement + 'last-screenshot.png', 'wb') as f:
             f.write(base64.b64decode(testimage))
 
     # Goto the edit page, add our ignore text
