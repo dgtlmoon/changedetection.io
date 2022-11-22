463747d3b7ae410387f403771355429f9b17c898
dgtlmoon <dgtlmoon@gmail.com>
2022-11-22 18:09:25 +0100

    0.39.22.1

--- changedetectionio/__init__.py
+++ changedetectionio/__init__.py
@@ -33,7 +33,7 @@ from flask_wtf import CSRFProtect
 from changedetectionio import html_tools
 from changedetectionio.api import api_v1
 
-__version__ = '0.39.22'
+__version__ = '0.39.22.1'
 
 datastore = None
 
