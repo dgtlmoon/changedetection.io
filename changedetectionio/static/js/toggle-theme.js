e6f8b44c27082e765b9cf7af6e4fee7138117ff7
Tanc <mail@tanc.me>
2022-12-04 15:49:21 +0100

    Use cookies instead of store for darkmode (#1188)

--- changedetectionio/static/js/toggle-theme.js
+++ changedetectionio/static/js/toggle-theme.js
@@ -3,24 +3,22 @@
  * Toggles theme between light and dark mode.
  */
 $(document).ready(function () {
-  const url = "/toggle-darkmode-state";
-
   const button = document.getElementsByClassName("toggle-theme")[0];
 
   button.onclick = () => {
-    fetch(url)
-      .then(function () {
-        const htmlElement = document.getElementsByTagName("html");
-        const isDarkMode = htmlElement[0].dataset.darkmode === "true";
-        htmlElement[0].dataset.darkmode = !isDarkMode;
-        if (isDarkMode) {
-          button.classList.remove("dark");
-        } else {
-          button.classList.add("dark");
-        }
-      })
-      .catch(function (e) {
-        console.log("Can't toggle the theme. Error was: ", e);
-      });
+    const htmlElement = document.getElementsByTagName("html");
+    const isDarkMode = htmlElement[0].dataset.darkmode === "true";
+    htmlElement[0].dataset.darkmode = !isDarkMode;
+    if (isDarkMode) {
+      button.classList.remove("dark");
+      setCookieValue(false);
+    } else {
+      button.classList.add("dark");
+      setCookieValue(true);
+    }
   };
+
+  const setCookieValue = (value) => {
+    document.cookie = `css_dark_mode=${value};max-age=31536000`
+  }
 });
