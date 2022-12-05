b53ef44d43cfa490c339dbbe0957b3574636edcd
dgtlmoon <dgtlmoon@gmail.com>
2022-12-05 16:56:38 +0100

    test fix - use local address

--- changedetectionio/tests/test_notification_errors.py
+++ changedetectionio/tests/test_notification_errors.py
@@ -11,23 +11,24 @@ def test_check_notification_error_handling(client, live_server):
     set_original_response()
 
     # Give the endpoint time to spin up
-    time.sleep(3)
+    time.sleep(2)
 
-    # use a different URL so that it doesnt interfere with the actual check until we are ready
+
+    # Set a URL and fetch it, then set a notification URL which is going to give errors
     test_url = url_for('test_endpoint', _external=True)
     res = client.post(
         url_for("form_quick_watch_add"),
-        data={"url": "https://changedetection.io/CHANGELOG.txt", "tag": ''},
+        data={"url": test_url, "tag": ''},
         follow_redirects=True
     )
     assert b"Watch added" in res.data
 
-    time.sleep(10)
-
+    time.sleep(2)
+    set_modified_response()
     # Check we capture the failure, we can just use trigger_check = y here
     res = client.post(
         url_for("edit_page", uuid="first"),
-        data={"notification_urls": "jsons://broken-url.changedetection.io/test",
+        data={"notification_urls": "jsons://broken-url-xxxxxxxx123/test",
               "notification_title": "xxx",
               "notification_body": "xxxxx",
               "notification_format": "Text",
@@ -62,5 +63,5 @@ def test_check_notification_error_handling(client, live_server):
         url_for("notification_logs"))
     assert bytes("Name or service not known".encode('utf-8')) in res.data
 
-
-    # And it should be listed on the watch overview
+    client.get(url_for("form_delete", uuid="all"), follow_redirects=True)
+    
