afb9a7fb9d695eadbbf364db53f0daba5655fe0e
dgtlmoon <dgtlmoon@gmail.com>
2022-12-17 17:11:28 +0100

    pdf detect cleanups

--- changedetectionio/content_fetcher.py
+++ changedetectionio/content_fetcher.py
@@ -559,7 +559,10 @@ class html_requests(Fetcher):
         # For example - some sites don't tell us it's utf-8, but return utf-8 content
         # This seems to not occur when using webdriver/selenium, it seems to detect the text encoding more reliably.
         # https://github.com/psf/requests/issues/1604 good info about requests encoding detection
-        if not r.headers.get('content-type','') == 'application/pdf':
+        is_pdf = 'application/pdf' in r.headers.get('content-type','') or url.lower().endswith('.pdf')
+        # is_pdf could be made into some call-argument like "is_binary"
+        if not is_pdf:
+            # Don't run this for PDF (maybe other big binaries?) takes a _long_ time
             if not r.headers.get('content-type') or not 'charset=' in r.headers.get('content-type'):
                 encoding = chardet.detect(r.content)['encoding']
                 if encoding:
@@ -575,7 +578,7 @@ class html_requests(Fetcher):
             raise Non200ErrorCodeReceived(url=url, status_code=r.status_code, page_html=r.text)
 
         self.status_code = r.status_code
-        if r.headers.get('content-type', '') == 'application/pdf':
+        if is_pdf:
             # So that `pdftotext` CLI can be called only if the content changes
             self.content = hashlib.md5(r.content).hexdigest()
         else:
