195ec091f550dedc500cea8e19756dea7e09cd3c
eitam <eitamzim@mail.huji.ac.il>
2023-02-01 13:14:59 +0200

    notifications fail on utf8 chars fix

--- changedetectionio/diff.py
+++ changedetectionio/diff.py
@@ -32,13 +32,15 @@ def customSequenceMatcher(before, after, include_equal=False):
 # only_differences - only return info about the differences, no context
 # line_feed_sep could be "<br/>" or "<li>" or "\n" etc
 def render_diff(previous_file, newest_file, include_equal=False, line_feed_sep="\n"):
-    with open(newest_file, 'r') as f:
+    with open(newest_file, 'r',encoding='utf-8') as f:
         newest_version_file_contents = f.read()
+        newest_version_file_contents = newest_version_file_contents.encode("ascii","ignore").decode("ascii")
         newest_version_file_contents = [line.rstrip() for line in newest_version_file_contents.splitlines()]
 
     if previous_file:
-        with open(previous_file, 'r') as f:
+        with open(previous_file, 'r',encoding='utf-8') as f:
             previous_version_file_contents = f.read()
+            previous_version_file_contents = previous_version_file_contents.encode("ascii","ignore").decode("ascii")
             previous_version_file_contents = [line.rstrip() for line in previous_version_file_contents.splitlines()]
     else:
         previous_version_file_contents = ""
