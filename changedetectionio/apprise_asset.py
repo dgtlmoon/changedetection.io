0e9ba9d72a90a450be18c7bf68ceb93522f884f9
dgtlmoon <dgtlmoon@gmail.com>
2023-03-22 12:53:53 +0100

    Re #1486

--- changedetectionio/apprise_asset.py
+++ changedetectionio/apprise_asset.py
@@ -3,7 +3,8 @@ import apprise
 # Create our AppriseAsset and populate it with some of our new values:
 # https://github.com/caronc/apprise/wiki/Development_API#the-apprise-asset-object
 asset = apprise.AppriseAsset(
-   image_url_logo='https://raw.githubusercontent.com/dgtlmoon/changedetection.io/master/changedetectionio/static/images/avatar-256x256.png'
+   image_url_logo='https://raw.githubusercontent.com/dgtlmoon/changedetection.io/master/changedetectionio/static/images/avatar-256x256.png',
+   async_mode=False  # Maybe related to https://github.com/dgtlmoon/changedetection.io/issues/1486
 )
 
 asset.app_id = "changedetection.io"
