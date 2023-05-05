512aa83ef93027c2bcca99af6e80994867291096
dgtlmoon <dgtlmoon@gmail.com>
2023-05-05 21:35:43 +0200

    tighter logic so test passes

--- changedetectionio/content_fetcher.py
+++ changedetectionio/content_fetcher.py
@@ -289,20 +289,18 @@ class base_html_playwright(Fetcher):
 
         # Fallback for now to the old way if browsersteps
         # @todo - need to figure out how to get browsersteps with images on each step working
-        for step in self.browser_steps:
-            if step['operation'] != None:
-                self.run_playwright(
-                               url,
-                               timeout,
-                               request_headers,
-                               request_body,
-                               request_method,
-                               ignore_status_codes,
-                               current_include_filters,
-                               is_binary)
-                return
-
-
+        if self.browser_steps:
+            for step in self.browser_steps:
+                if step.get('operation'):
+                    return self.run_playwright(
+                                   url,
+                                   timeout,
+                                   request_headers,
+                                   request_body,
+                                   request_method,
+                                   ignore_status_codes,
+                                   current_include_filters,
+                                   is_binary)
 
         extra_wait_ms = (int(os.getenv("WEBDRIVER_DELAY_BEFORE_CONTENT_READY", 5)) + self.render_extract_delay) * 1000
         xpath_element_js = self.xpath_element_js.replace('%ELEMENTS%', visualselector_xpath_selectors)
