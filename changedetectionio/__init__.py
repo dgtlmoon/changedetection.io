7c97a5a4039128b83d6debc68e8489009fd7eca6
dgtlmoon <dgtlmoon@gmail.com>
2023-11-12 12:05:54 +0100

    0.45.7.3

--- changedetectionio/__init__.py
+++ changedetectionio/__init__.py
@@ -38,7 +38,7 @@ from flask_paginate import Pagination, get_page_parameter
 from changedetectionio import html_tools
 from changedetectionio.api import api_v1
 
-__version__ = '0.45.7.2'
+__version__ = '0.45.7.3'
 
 from changedetectionio.store import BASE_URL_NOT_SET_TEXT
 
