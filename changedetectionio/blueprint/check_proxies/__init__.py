dfe26cb486c21347419ef4dfdbc29a6111bde93b
dgtlmoon <dgtlmoon@gmail.com>
2023-11-13 10:12:07 +0100

    Fixing proxy check call

--- changedetectionio/blueprint/check_proxies/__init__.py
+++ changedetectionio/blueprint/check_proxies/__init__.py
@@ -41,7 +41,7 @@ def construct_blueprint(datastore: ChangeDetectionStore):
         now = time.time()
         try:
             update_handler = text_json_diff.perform_site_check(datastore=datastore, watch_uuid=uuid)
-            changed_detected, update_obj, contents = update_handler.call_browser()
+            update_handler.call_browser()
         # title, size is len contents not len xfer
         except content_fetcher.Non200ErrorCodeReceived as e:
             if e.status_code == 404:
