3004e2fffe24063686c2fa1b474eb9594c133613
dgtlmoon <dgtlmoon@gmail.com>
2023-11-28 16:47:35 +0100

    tweak settings

--- changedetectionio/static/js/vis.js
+++ changedetectionio/static/js/vis.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $('[data-visible-for]').hide();
     $(':radio').on('keyup keypress blur change click', function (e) {
         $('[data-visible-for]').hide();
+        $('.advanced-options').hide();
         var n = $(this).attr('name') + "=" + $(this).val();
         if (n === 'fetch_backend=system') {
             n = "fetch_backend=" + default_system_fetch_backend;
@@ -12,4 +13,10 @@ $(document).ready(function () {
 
     });
     $(':radio:checked').change();
+
+
+    // Show advanced
+    $('.show-advanced').click(function (e) {
+        $(this).closest('.tab-pane-inner').find('.advanced-options').toggle();
+    });
 });
