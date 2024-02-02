c30f96c4cdac0452f88fedf79b3144e603d3771c
dgtlmoon <dgtlmoon@gmail.com>
2024-02-02 09:45:09 +0100

    update test

--- changedetectionio/tests/test_obfuscations.py
+++ changedetectionio/tests/test_obfuscations.py
@@ -2,7 +2,7 @@
 
 import time
 from flask import url_for
-from .util import live_server_setup
+from .util import live_server_setup, wait_for_all_checks
 
 
 def set_original_ignore_response():
@@ -21,7 +21,7 @@ def set_original_ignore_response():
 def test_obfuscations(client, live_server):
     set_original_ignore_response()
     live_server_setup(live_server)
-    time.sleep(1)
+
     # Add our URL to the import page
     test_url = url_for('test_endpoint', _external=True)
     res = client.post(
@@ -32,12 +32,12 @@ def test_obfuscations(client, live_server):
     assert b"1 Imported" in res.data
 
     # Give the thread time to pick it up
-    time.sleep(3)
+    wait_for_all_checks(client)
 
     # Check HTML conversion detected and workd
     res = client.get(
         url_for("preview_page", uuid="first"),
         follow_redirects=True
     )
-
-    assert b'$90.74' in res.data
+    # whitespace appears but it renders https://github.com/weblyzard/inscriptis/issues/45#issuecomment-1923339265
+    assert b'$ 90.74' in res.data
