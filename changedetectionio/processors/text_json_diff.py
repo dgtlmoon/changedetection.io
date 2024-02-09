16dbd69f575603d834e56cd9bc5c7333284b82d3
Constantin Hong <hongconstantin@gmail.com>
2024-02-10 01:49:11 +0900

    text_json_diff/refactor: Add merger_filters function

--- changedetectionio/processors/text_json_diff.py
+++ changedetectionio/processors/text_json_diff.py
@@ -28,6 +28,8 @@ class PDFToHTMLToolNotFound(ValueError):
     def __init__(self, msg):
         ValueError.__init__(self, msg)
 
+def merge_filters(include_filters, include_filters_from_tags):
+    return list({*include_filters, *include_filters_from_tags})
 
 # Some common stuff here that can be moved to a base class
 # (set_proxy_from_list)
@@ -118,7 +120,7 @@ class perform_site_check(difference_detection_processor):
         include_filters_from_tags = self.datastore.get_tag_overrides_for_watch(uuid=uuid, attr='include_filters')
 
         # 1845 - remove duplicated filters in both group and watch include filter
-        include_filters_rule = list({*watch.get('include_filters', []), *include_filters_from_tags})
+        include_filters_rule = merge_filters(watch.get('include_filters', []), include_filters_from_tags)
 
         subtractive_selectors = [*self.datastore.get_tag_overrides_for_watch(uuid=uuid, attr='subtractive_selectors'),
                                  *watch.get("subtractive_selectors", []),
