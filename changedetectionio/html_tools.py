1747225c67c3097a393f797ed71a27370f32039e
Constantin Hong <hongconstantin@gmail.com>
2024-02-11 01:41:35 +0900

    html_tools/feat: Add parameter fragment explicitly into elementpath.select

--- changedetectionio/html_tools.py
+++ changedetectionio/html_tools.py
@@ -125,7 +125,12 @@ def xpath_filter(xpath_filter, html_content, append_pretty_line_formatting=False
     tree = html.fromstring(bytes(html_content, encoding='utf-8'), parser=parser)
     html_block = ""
 
-    r = elementpath.select(tree, xpath_filter.strip(), namespaces={'re': 'http://exslt.org/regular-expressions'}, parser=XPath3Parser)
+    # https://github.com/sissaschool/elementpath/issues/71
+    r = elementpath.select(tree,
+            xpath_filter.strip(),
+            namespaces={'re': 'http://exslt.org/regular-expressions'},
+            fragment=False,
+            parser=XPath3Parser)
     #@note: //title/text() wont work where <title>CDATA..
 
     if type(r) != list:
