52c895b2e8009977ad60dfc2b715f3ecbeeac738
Constantin Hong <hongconstantin@gmail.com>
2024-02-21 19:46:23 +0900

    text_json_diff/fix: Keep an order of filter and remove duplicated filters. 2 (#2178)

--- changedetectionio/processors/text_json_diff.py
+++ changedetectionio/processors/text_json_diff.py
@@ -119,7 +119,7 @@ class perform_site_check(difference_detection_processor):
         include_filters_from_tags = self.datastore.get_tag_overrides_for_watch(uuid=uuid, attr='include_filters')
 
         # 1845 - remove duplicated filters in both group and watch include filter
-        include_filters_rule = list({*watch.get('include_filters', []), *include_filters_from_tags})
+        include_filters_rule = list(dict.fromkeys(watch.get('include_filters', []) + include_filters_from_tags))
 
         subtractive_selectors = [*self.datastore.get_tag_overrides_for_watch(uuid=uuid, attr='subtractive_selectors'),
                                  *watch.get("subtractive_selectors", []),
