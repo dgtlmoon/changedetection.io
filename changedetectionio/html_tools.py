66a7dae381367ede985202598913b5e97019b5fb
Constantin Hong <hongconstantin@gmail.com>
2024-05-08 03:00:50 +0900

    html_tools/docs: Fix old comment

--- changedetectionio/html_tools.py
+++ changedetectionio/html_tools.py
@@ -113,7 +113,7 @@ def elementpath_tostring(obj):
 def forest_transplanting(root):
     """
     libxml2 violates DOM rules. it means there can be multiple root element
-    nodes. So I choose just transplating them to a new root by default.
+    nodes. So I choose just transplating them to a new root when the violation happens.
     See also, https://gitlab.gnome.org/GNOME/libxml2/-/issues/716
     This will emulate xpath1 of html of libxml2 like '/html[2]/*'.
     To make this function work, 'fragment=True' in elementpath.select is required.
