35f9bdc8b0d5e3c343cea1c3978067ac73e93011
dgtlmoon <dgtlmoon@gmail.com>
2024-06-07 09:14:40 +0200

    NFI

--- changedetectionio/tests/util.py
+++ changedetectionio/tests/util.py
@@ -124,13 +124,12 @@ def extract_UUID_from_client(client):
 def wait_for_all_checks(client):
     # Loop waiting until done..
     attempt = 0
-    time.sleep(1)
     while attempt < 60:
+        time.sleep(1)
         res = client.get(url_for("index"))
         if not b'Checking now' in res.data:
             break
         logging.getLogger().info("Waiting for watch-list to not say 'Checking now'.. {}".format(attempt))
-        time.sleep(1)
         attempt += 1
 
 def live_server_setup(live_server):
