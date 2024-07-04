38e1ee80965f39ea9c5eaaeb5e14a5a08a4d6afe
dgtlmoon <dgtlmoon@gmail.com>
2024-07-04 14:03:06 +0200

    Adding test

--- changedetectionio/update_worker.py
+++ changedetectionio/update_worker.py
@@ -249,7 +249,7 @@ class update_worker(threading.Thread):
 
                     # Clear last errors (move to preflight func?)
                     self.datastore.data['watching'][uuid]['browser_steps_last_error_step'] = None
-                                        
+
                     watch = self.datastore.data['watching'].get(uuid)
 
                     logger.info(f"Processing watch UUID {uuid} Priority {queued_item_data.priority} URL {watch['url']}")
