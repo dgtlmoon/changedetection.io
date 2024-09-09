337fcab3f1afeeab066282ccdde7c3d3e0e2d33d
dgtlmoon <dgtlmoon@gmail.com>
2024-09-09 16:50:00 +0200

    Testing/Code - Improving test reliability (#2617)

--- changedetectionio/tests/test_filter_exist_changes.py
+++ changedetectionio/tests/test_filter_exist_changes.py
@@ -4,7 +4,7 @@
 import os
 import time
 from flask import url_for
-from .util import set_original_response, live_server_setup
+from .util import set_original_response, live_server_setup, wait_for_notification_endpoint_output
 from changedetectionio.model import App
 
 
@@ -102,14 +102,15 @@ def test_filter_doesnt_exist_then_exists_should_get_notification(client, live_se
         follow_redirects=True
     )
     assert b"Updated watch." in res.data
-    time.sleep(3)
+    wait_for_notification_endpoint_output()
 
     # Shouldn't exist, shouldn't have fired
     assert not os.path.isfile("test-datastore/notification.txt")
     # Now the filter should exist
     set_response_with_filter()
     client.get(url_for("form_watch_checknow"), follow_redirects=True)
-    time.sleep(3)
+
+    wait_for_notification_endpoint_output()
 
     assert os.path.isfile("test-datastore/notification.txt")
 
