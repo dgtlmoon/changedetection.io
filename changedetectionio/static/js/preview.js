bec8ad229263eda73a754b7a8b997c8bfcb25827
dgtlmoon <dgtlmoon@gmail.com>
2024-10-05 13:29:55 +0200

    Tidy up the JS

--- changedetectionio/static/js/preview.js
+++ changedetectionio/static/js/preview.js
@@ -1,69 +1,63 @@
-function redirect_to_version(version) {
-    var currentUrl = window.location.href;
-    var baseUrl = currentUrl.split('?')[0]; // Base URL without query parameters
+function redirectToVersion(version) {
+    var currentUrl = window.location.href.split('?')[0]; // Base URL without query parameters
     var anchor = '';
 
     // Check if there is an anchor
-    if (baseUrl.indexOf('#') !== -1) {
-        anchor = baseUrl.substring(baseUrl.indexOf('#'));
-        baseUrl = baseUrl.substring(0, baseUrl.indexOf('#'));
+    if (currentUrl.indexOf('#') !== -1) {
+        anchor = currentUrl.substring(currentUrl.indexOf('#'));
+        currentUrl = currentUrl.substring(0, currentUrl.indexOf('#'));
     }
-    window.location.href = baseUrl + '?version=' + version + anchor;
+
+    window.location.href = currentUrl + '?version=' + version + anchor;
 }
 
-function setup_date_widget() {
-    document.addEventListener('keydown', function (event) {
-        var selectElement = document.getElementById('preview-version');
-        if (selectElement) {
-            var selectedOption = selectElement.querySelector('option:checked');
-            if (selectedOption) {
-                if (event.key === 'ArrowLeft') {
-                    if (selectedOption.previousElementSibling) {
-                        redirect_to_version(selectedOption.previousElementSibling.value);
-                    }
-                } else if (event.key === 'ArrowRight') {
-                    if (selectedOption.nextElementSibling) {
-                        redirect_to_version(selectedOption.nextElementSibling.value);
-                    }
-                }
+function setupDateWidget() {
+    $(document).on('keydown', function (event) {
+        var $selectElement = $('#preview-version');
+        var $selectedOption = $selectElement.find('option:selected');
+
+        if ($selectedOption.length) {
+            if (event.key === 'ArrowLeft' && $selectedOption.prev().length) {
+                redirectToVersion($selectedOption.prev().val());
+            } else if (event.key === 'ArrowRight' && $selectedOption.next().length) {
+                redirectToVersion($selectedOption.next().val());
             }
         }
     });
 
-
-    document.getElementById('preview-version').addEventListener('change', function () {
-        redirect_to_version(this.value);
+    $('#preview-version').on('change', function () {
+        redirectToVersion($(this).val());
     });
 
-    var selectElement = document.getElementById('preview-version');
-    if (selectElement) {
-        var selectedOption = selectElement.querySelector('option:checked');
-        if (selectedOption) {
-            if (selectedOption.previousElementSibling) {
-                document.getElementById('btn-previous').href = "?version=" + selectedOption.previousElementSibling.value;
-            } else {
-                document.getElementById('btn-previous').remove()
-            }
-            if (selectedOption.nextElementSibling) {
-                document.getElementById('btn-next').href = "?version=" + selectedOption.nextElementSibling.value;
-            } else {
-                document.getElementById('btn-next').remove()
-            }
+    var $selectedOption = $('#preview-version option:selected');
 
+    if ($selectedOption.length) {
+        var $prevOption = $selectedOption.prev();
+        var $nextOption = $selectedOption.next();
+
+        if ($prevOption.length) {
+            $('#btn-previous').attr('href', '?version=' + $prevOption.val());
+        } else {
+            $('#btn-previous').remove();
+        }
+
+        if ($nextOption.length) {
+            $('#btn-next').attr('href', '?version=' + $nextOption.val());
+        } else {
+            $('#btn-next').remove();
         }
     }
 }
 
 $(document).ready(function () {
     if ($('#preview-version').length) {
-        setup_date_widget()
+        setupDateWidget();
     }
 
-    $('#diff-col> pre')
-        .highlightLines([
-            {
-                'color': '#ee0000',
-                'lines': triggered_line_numbers
-            }
-        ]);
+    $('#diff-col > pre').highlightLines([
+        {
+            'color': '#ee0000',
+            'lines': triggered_line_numbers
+        }
+    ]);
 });
