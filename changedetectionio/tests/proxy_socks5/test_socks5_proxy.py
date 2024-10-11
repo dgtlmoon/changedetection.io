a4b3a752f2f80f95a9401c28d9008fcecdf4b6e2
dgtlmoon <dgtlmoon@gmail.com>
2024-10-11 11:10:46 +0200

    woops

--- changedetectionio/tests/proxy_socks5/test_socks5_proxy.py
+++ changedetectionio/tests/proxy_socks5/test_socks5_proxy.py
@@ -20,7 +20,7 @@ def set_response():
 
 
 def test_socks5(client, live_server, measure_memory_usage):
-   # live_server_setup(live_server)
+    live_server_setup(live_server)
     set_response()
 
     # Setup a proxy
