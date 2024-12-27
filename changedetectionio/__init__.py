6adf10597edfd9e4c5336af0986332a580f3128e
dgtlmoon <dgtlmoon@gmail.com>
2024-12-27 11:24:56 +0100

    0.48.05

--- changedetectionio/__init__.py
+++ changedetectionio/__init__.py
@@ -2,7 +2,7 @@
 
 # Read more https://github.com/dgtlmoon/changedetection.io/wiki
 
-__version__ = '0.48.04'
+__version__ = '0.48.05'
 
 from changedetectionio.strtobool import strtobool
 from json.decoder import JSONDecodeError
