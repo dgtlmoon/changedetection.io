3214801a204867fd7bdc6c0b87772e9e9d4f4d4a
Luca Cirillo <luca.cirillo5@gmail.com>
2025-03-25 10:54:42 +0100

    chore(apprise): increased readability and optimized dict access in authentication process
    chg(apprise): create headers and params dict in place

--- changedetectionio/apprise_plugin/__init__.py
+++ changedetectionio/apprise_plugin/__init__.py
@@ -9,6 +9,19 @@ from loguru import logger
 from requests.structures import CaseInsensitiveDict
 
 
+def _get_auth(parsed_url: dict) -> str | tuple[str, str]:
+    auth_user: str | None = parsed_url.get("user")
+    auth_password: str | None = parsed_url.get("password")
+    
+    if auth_user is not None and auth_password is not None:
+        return (unquote_plus(auth_user), unquote_plus(auth_password))
+    
+    if auth_user is not None:
+        return unquote_plus(auth_user)
+    
+    return ""
+
+
 @notify(on="get")
 @notify(on="gets")
 @notify(on="post")
@@ -29,32 +42,26 @@ def apprise_custom_api_call_wrapper(
 ) -> bool:
     url: str = meta.get("url")
     schema: str = meta.get("schema").lower().strip()
-    method: str = re.sub(r"s$", "", schema).upper()
-
-    params = CaseInsensitiveDict({}) # Added to requests
-    auth = None
-    has_error = False
 
     # Convert /foobar?+some-header=hello to proper header dictionary
     parsed_url: dict[str, str | dict | None] = apprise_parse_url(url)
 
-    # Add our headers that the user can potentially over-ride if they wish
-    # to to our returned result set and tidy entries by unquoting them
-    headers = CaseInsensitiveDict({unquote_plus(x): unquote_plus(y)
-               for x, y in parsed_url['qsd+'].items()})
+    headers = CaseInsensitiveDict(
+        {unquote_plus(k): unquote_plus(v) for k, v in parsed_url["qsd+"].items()}
+    )
 
     # https://github.com/caronc/apprise/wiki/Notify_Custom_JSON#get-parameter-manipulation
     # In Apprise, it relies on prefixing each request arg with "-", because it uses say &method=update as a flag for apprise
     # but here we are making straight requests, so we need todo convert this against apprise's logic
-    for k, v in parsed_url['qsd'].items():
-        if not k.strip('+-') in parsed_url['qsd+'].keys():
-            params[unquote_plus(k)] = unquote_plus(v)
+    params = CaseInsensitiveDict(
+        {
+            unquote_plus(k): unquote_plus(v)
+            for k, v in parsed_url["qsd"].items()
+            if k.strip("+-") not in parsed_url["qsd+"]
+        }
+    )
 
-    auth = ""
-    if parsed_url.get('user') and parsed_url.get('password'):
-        auth = (unquote_plus(parsed_url.get('user')), unquote_plus(parsed_url.get('password')))
-    elif parsed_url.get('user'):
-        auth = (unquote_plus(parsed_url.get('user')))
+    auth = _get_auth(parsed_url=parsed_url)
 
     # If it smells like it could be JSON and no content-type was already set, offer a default content type.
     if body and '{' in body[:100] and not headers.get('Content-Type'):
@@ -74,6 +81,9 @@ def apprise_custom_api_call_wrapper(
         url = re.sub(rf'^{schema}', 'http', parsed_url.get('url'))
 
     status_str = ''
+    has_error = False
+    method: str = re.sub(r"s$", "", schema).upper()
+
     try:
         r = requests.request(
             method=method,
