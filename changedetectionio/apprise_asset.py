38d1b2d3c0be596f4445315d4b27cf37f0966a32
Luca Cirillo <luca.cirillo5@gmail.com>
2025-03-25 09:16:30 +0100

    chg(apprise): use constants so it's easy to reuse assets values, pass them directly into the constructor of AppriseAsset
    Added some basic tests to validate those changes

--- changedetectionio/apprise_asset.py
+++ changedetectionio/apprise_asset.py
@@ -1,12 +1,16 @@
-from changedetectionio import apprise_plugin
-import apprise
+from apprise import AppriseAsset
 
-# Create our AppriseAsset and populate it with some of our new values:
+# Refer to:
 # https://github.com/caronc/apprise/wiki/Development_API#the-apprise-asset-object
-asset = apprise.AppriseAsset(
-   image_url_logo='https://raw.githubusercontent.com/dgtlmoon/changedetection.io/master/changedetectionio/static/images/avatar-256x256.png'
-)
 
-asset.app_id = "changedetection.io"
-asset.app_desc = "ChangeDetection.io best and simplest website monitoring and change detection"
-asset.app_url = "https://changedetection.io"
+APPRISE_APP_ID = "changedetection.io"
+APPRISE_APP_DESC = "ChangeDetection.io best and simplest website monitoring and change detection"
+APPRISE_APP_URL = "https://changedetection.io"
+APPRISE_AVATAR_URL = "https://raw.githubusercontent.com/dgtlmoon/changedetection.io/master/changedetectionio/static/images/avatar-256x256.png"
+
+asset = AppriseAsset(
+    app_id=APPRISE_APP_ID,
+    app_desc=APPRISE_APP_DESC,
+    app_url=APPRISE_APP_URL,
+    image_url_logo=APPRISE_AVATAR_URL,
+)
