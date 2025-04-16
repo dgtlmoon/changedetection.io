d1ef5632db67856d1a7aefba3cc8ed8c3625f151
dgtlmoon <dgtlmoon@gmail.com>
2025-04-16 18:14:48 +0200

    Revert "UI Edit/Stats - Add levenshtein distance info, explains how "different" the last two snapshot are (#3109)"
    This reverts commit cfb4decf676223fb6d6e593e2af5e9ce459254ef.

--- changedetectionio/blueprint/ui/edit.py
+++ changedetectionio/blueprint/ui/edit.py
@@ -19,20 +19,6 @@ def construct_blueprint(datastore: ChangeDetectionStore, update_q, queuedWatchMe
             if tag_uuid in watch.get('tags', []) and (tag.get('include_filters') or tag.get('subtractive_selectors')):
                 return True
 
-    def levenshtein_ratio_recent_history(watch):
-        try:
-            from Levenshtein import ratio, distance
-            k = list(watch.history.keys())
-            if len(k) >= 2:
-                a = watch.get_history_snapshot(timestamp=k[0])
-                b = watch.get_history_snapshot(timestamp=k[1])
-                distance = distance(a, b)
-                return distance
-        except Exception as e:
-            logger.warning("Unable to calc similarity", e)
-            return "Unable to calc similarity"
-        return ''
-
     @edit_blueprint.route("/edit/<string:uuid>", methods=['GET', 'POST'])
     @login_optionally_required
     # https://stackoverflow.com/questions/42984453/wtforms-populate-form-with-data-if-data-exists
@@ -260,7 +246,6 @@ def construct_blueprint(datastore: ChangeDetectionStore, update_q, queuedWatchMe
                 'has_extra_headers_file': len(datastore.get_all_headers_in_textfile_for_watch(uuid=uuid)) > 0,
                 'has_special_tag_options': _watch_has_tag_options_set(watch=watch),
                 'jq_support': jq_support,
-                'lev_info': levenshtein_ratio_recent_history(watch),
                 'playwright_enabled': os.getenv('PLAYWRIGHT_DRIVER_URL', False),
                 'settings_application': datastore.data['settings']['application'],
                 'system_has_playwright_configured': os.getenv('PLAYWRIGHT_DRIVER_URL'),
