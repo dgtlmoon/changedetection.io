9b7372fff047e6a4d9f0c7c585ec7c8257ec937c
Jost Alemann <58050402+ddogfoodd@users.noreply.github.com>
2025-05-22 10:11:39 +0000

    Code - Remove unused f-strings (#3209)

--- changedetectionio/processors/__init__.py
+++ changedetectionio/processors/__init__.py
@@ -89,7 +89,7 @@ class difference_detection_processor():
                 proxy_url = self.datastore.proxy_list.get(preferred_proxy_id).get('url')
                 logger.debug(f"Selected proxy key '{preferred_proxy_id}' as proxy URL '{proxy_url}' for {url}")
             else:
-                logger.debug(f"Skipping adding proxy data when custom Browser endpoint is specified. ")
+                logger.debug("Skipping adding proxy data when custom Browser endpoint is specified. ")
 
         # Now call the fetcher (playwright/requests/etc) with arguments that only a fetcher would need.
         # When browser_connection_url is None, it method should default to working out whats the best defaults (os env vars etc)
