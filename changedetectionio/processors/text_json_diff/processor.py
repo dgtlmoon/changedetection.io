8c03b65dc674f7beea20892e60b0edd99073a693
dgtlmoon <dgtlmoon@gmail.com>
2025-09-07 15:33:18 +0200

    Fix - Filters in tags/groups were being added to watches on each check - #3406 fix list update (#3407)

--- changedetectionio/processors/text_json_diff/processor.py
+++ changedetectionio/processors/text_json_diff/processor.py
@@ -251,8 +251,7 @@ class perform_site_check(difference_detection_processor):
         update_obj["last_check_status"] = self.fetcher.get_last_status_code()
 
         # 615 Extract text by regex
-        extract_text = watch.get('extract_text', [])
-        extract_text += self.datastore.get_tag_overrides_for_watch(uuid=watch.get('uuid'), attr='extract_text')
+        extract_text = list(dict.fromkeys(watch.get('extract_text', []) + self.datastore.get_tag_overrides_for_watch(uuid=watch.get('uuid'), attr='extract_text')))
         if len(extract_text) > 0:
             regex_matched_output = []
             for s_re in extract_text:
@@ -311,8 +310,7 @@ class perform_site_check(difference_detection_processor):
 
         ############ Blocking rules, after checksum #################
         blocked = False
-        trigger_text = watch.get('trigger_text', [])
-        trigger_text += self.datastore.get_tag_overrides_for_watch(uuid=watch.get('uuid'), attr='trigger_text')
+        trigger_text = list(dict.fromkeys(watch.get('trigger_text', []) + self.datastore.get_tag_overrides_for_watch(uuid=watch.get('uuid'), attr='trigger_text')))
         if len(trigger_text):
             # Assume blocked
             blocked = True
@@ -326,8 +324,7 @@ class perform_site_check(difference_detection_processor):
             if result:
                 blocked = False
 
-        text_should_not_be_present = watch.get('text_should_not_be_present', [])
-        text_should_not_be_present += self.datastore.get_tag_overrides_for_watch(uuid=watch.get('uuid'), attr='text_should_not_be_present')
+        text_should_not_be_present = list(dict.fromkeys(watch.get('text_should_not_be_present', []) + self.datastore.get_tag_overrides_for_watch(uuid=watch.get('uuid'), attr='text_should_not_be_present')))
         if len(text_should_not_be_present):
             # If anything matched, then we should block a change from happening
             result = html_tools.strip_ignore_text(content=str(stripped_text_from_html),
