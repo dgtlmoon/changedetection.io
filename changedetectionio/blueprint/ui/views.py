1d3cadc77301f0d02bee4d438ffe65e170f7cad5
dgtlmoon <dgtlmoon@gmail.com>
2025-10-06 19:10:58 +0200

    back on

--- changedetectionio/blueprint/ui/views.py
+++ changedetectionio/blueprint/ui/views.py
@@ -224,7 +224,7 @@ def construct_blueprint(datastore: ChangeDetectionStore, update_q, queuedWatchMe
         content = diff.render_diff(from_version_file_contents,
                                    to_version_file_contents,
                                    include_equal=True,
-                                   html_colour=False,
+                                   html_colour=True,
                                    ignore_junk=datastore.data['settings']['application'].get('ignore_whitespace', False),
                                    )
 
