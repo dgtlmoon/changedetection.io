49fedf380d78f3ef27988fe54e68f0212310b89c
dgtlmoon <dgtlmoon@gmail.com>
2025-11-07 18:30:29 +0100

    WIP

--- changedetectionio/blueprint/rss/blueprint.py
+++ changedetectionio/blueprint/rss/blueprint.py
@@ -37,6 +37,47 @@ def clean_entry_content(content):
 def construct_blueprint(datastore: ChangeDetectionStore):
     rss_blueprint = Blueprint('rss', __name__)
 
+    # Helper function to generate GUID for RSS entries
+    def generate_watch_guid(watch):
+        """Generate a unique GUID for a watch RSS entry."""
+        return f"{watch['uuid']}/{watch.last_changed}"
+
+    # Helper function to generate diff content for a watch
+    def generate_watch_diff_content(watch, dates):
+        """
+        Generate HTML diff content for a watch given its history dates.
+        Returns the rendered HTML content ready for RSS/display.
+        """
+        from changedetectionio import diff
+
+        # Same logic as watch-overview.html
+        if datastore.data['settings']['application']['ui'].get('use_page_title_in_list') or watch.get('use_page_title_in_list'):
+            watch_label = watch.label
+        else:
+            watch_label = watch.get('url')
+
+        try:
+            html_diff = diff.render_diff(
+                previous_version_file_contents=watch.get_history_snapshot(timestamp=dates[-2]),
+                newest_version_file_contents=watch.get_history_snapshot(timestamp=dates[-1]),
+                include_equal=False
+            )
+
+            requested_output_format = datastore.data['settings']['application'].get('rss_content_format')
+            url, html_diff, n_title = apply_service_tweaks(url='', n_body=html_diff, n_title=None, requested_output_format=requested_output_format)
+
+        except FileNotFoundError as e:
+            html_diff = f"History snapshot file for watch {watch.get('uuid')}@{watch.last_changed} - '{watch.get('title')} not found."
+
+        rss_template = "<html><body>\n<h4><a href=\"{{watch_url}}\">{{watch_title}}</a></h4>\n<p>{{html_diff}}</p>\n</body></html>\n"
+        content = jinja_render(template_str=rss_template, watch_title=watch_label, html_diff=html_diff, watch_url=watch.link)
+
+        # Out of range chars could also break feedgen
+        if scan_invalid_chars_in_rss(content):
+            content = clean_entry_content(content)
+
+        return content, watch_label
+
     # Some RSS reader situations ended up with rss/ (forward slash after RSS) due
     # to some earlier blueprint rerouting work, it should goto feed.
     @rss_blueprint.route("/", methods=['GET'])
@@ -91,7 +132,7 @@ def construct_blueprint(datastore: ChangeDetectionStore):
             if not watch.viewed:
                 # Re #239 - GUID needs to be individual for each event
                 # @todo In the future make this a configurable link back (see work on BASE_URL https://github.com/dgtlmoon/changedetection.io/pull/228)
-                guid = "{}/{}".format(watch['uuid'], watch.last_changed)
+                guid = generate_watch_guid(watch)
                 fe = fg.add_entry()
 
                 # Include a link to the diff page, they will have to login here to see if password protection is enabled.
@@ -105,36 +146,9 @@ def construct_blueprint(datastore: ChangeDetectionStore):
 
                 fe.link(link=diff_link)
 
-                # Same logic as watch-overview.html
-                if datastore.data['settings']['application']['ui'].get('use_page_title_in_list') or watch.get('use_page_title_in_list'):
-                    watch_label = watch.label
-                else:
-                    watch_label = watch.get('url')
+                content, watch_label = generate_watch_diff_content(watch, dates)
 
                 fe.title(title=watch_label)
-                try:
-                    html_diff = diff.render_diff(previous_version_file_contents=watch.get_history_snapshot(timestamp=dates[-2]),
-                                                 newest_version_file_contents=watch.get_history_snapshot(timestamp=dates[-1]),
-                                                 include_equal=False,
-                                                 line_feed_sep="<br>"
-                                                 )
-
-                    requested_output_format = datastore.data['settings']['application'].get('rss_content_format')
-                    url, html_diff, n_title = apply_service_tweaks(url='', n_body=html_diff, n_title=None, requested_output_format=requested_output_format)
-
-                except FileNotFoundError as e:
-                    html_diff = f"History snapshot file for watch {watch.get('uuid')}@{watch.last_changed} - '{watch.get('title')} not found."
-
-                # @todo Make this configurable and also consider html-colored markup
-                # @todo User could decide if <link> goes to the diff page, or to the watch link
-                rss_template = "<html><body>\n<h4><a href=\"{{watch_url}}\">{{watch_title}}</a></h4>\n<p>{{html_diff}}</p>\n</body></html>\n"
-
-                content = jinja_render(template_str=rss_template, watch_title=watch_label, html_diff=html_diff, watch_url=watch.link)
-
-                # Out of range chars could also break feedgen
-                if scan_invalid_chars_in_rss(content):
-                    content = clean_entry_content(content)
-
                 fe.content(content=content, type='CDATA')
                 fe.guid(guid, permalink=False)
                 dt = datetime.datetime.fromtimestamp(int(watch.newest_history_key))
@@ -144,6 +158,59 @@ def construct_blueprint(datastore: ChangeDetectionStore):
         response = make_response(fg.rss_str())
         response.headers.set('Content-Type', 'application/rss+xml;charset=utf-8')
         logger.trace(f"RSS generated in {time.time() - now:.3f}s")
+        return response
+
+    @rss_blueprint.route("/watch/<string:uuid>", methods=['GET'])
+    def rss_single_watch(uuid):
+        """
+        Display the most recent change for a single watch as RSS feed.
+        Returns RSS XML with a single entry showing the diff between the last two snapshots.
+        """
+        # Always requires token set
+        app_rss_token = datastore.data['settings']['application'].get('rss_access_token')
+        rss_url_token = request.args.get('token')
+        if rss_url_token != app_rss_token:
+            return "Access denied, bad token", 403
+
+        # Get the watch by UUID
+        watch = datastore.data['watching'].get(uuid)
+        if not watch:
+            return f"Watch with UUID {uuid} not found", 404
+
+        # Check if watch has at least 2 history snapshots
+        dates = list(watch.history.keys())
+        if len(dates) < 2:
+            return f"Watch {uuid} does not have enough history snapshots to show changes (need at least 2)", 400
+
+        # Add uuid to watch for proper functioning
+        watch['uuid'] = uuid
+
+        # Generate the diff content using the shared helper function
+        content, watch_label = generate_watch_diff_content(watch, dates)
+
+        # Create RSS feed with single entry
+        fg = FeedGenerator()
+        fg.title(f'changedetection.io - {watch.label}')
+        fg.description('Changes')
+        fg.link(href='https://changedetection.io')
+
+        # Add single entry for this watch
+        guid = generate_watch_guid(watch)
+        fe = fg.add_entry()
+
+        # Include a link to the diff page
+        diff_link = {'href': url_for('ui.ui_views.diff_history_page', uuid=watch['uuid'], _external=True)}
+        fe.link(link=diff_link)
+
+        fe.title(title=watch_label)
+        fe.content(content=content, type='CDATA')
+        fe.guid(guid, permalink=False)
+        dt = datetime.datetime.fromtimestamp(int(watch.newest_history_key))
+        dt = dt.replace(tzinfo=pytz.UTC)
+        fe.pubDate(dt)
+
+        response = make_response(fg.rss_str())
+        response.headers.set('Content-Type', 'application/rss+xml;charset=utf-8')
         return response
 
     return rss_blueprint
