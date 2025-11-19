431f64f75630a807ef4f79a1c9d2297e7e9cd4ee
dgtlmoon <dgtlmoon@gmail.com>
2025-11-19 09:20:57 +0100

    needs check

--- changedetectionio/tests/test_notification.py
+++ changedetectionio/tests/test_notification.py
@@ -532,6 +532,7 @@ def test_single_send_test_notification_on_watch(client, live_server, measure_mem
     test_url = url_for('test_endpoint', _external=True)
     uuid = client.application.config.get('DATASTORE').add_watch(url=test_url)
     client.get(url_for("ui.form_watch_checknow"), follow_redirects=True)
+    wait_for_all_checks(client)
 
     test_notification_url = url_for('test_notification_endpoint', _external=True).replace('http://', 'post://')+"?xxx={{ watch_url }}&+custom-header=123"
     # 1995 UTF-8 content should be encoded
