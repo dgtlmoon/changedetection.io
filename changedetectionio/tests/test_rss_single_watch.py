5671e1a75818db07d2935e17075f052035b1542b
dgtlmoon <dgtlmoon@gmail.com>
2025-11-19 11:53:26 +0100

    Adding extra_notification_token_placeholder_info=extra_notification_token_placeholder_info

--- changedetectionio/tests/test_rss_single_watch.py
+++ changedetectionio/tests/test_rss_single_watch.py
@@ -345,3 +345,4 @@ def test_rss_single_watch_follow_notification_body(client, live_server, measure_
     )
     item_description = ET.fromstring(res.data).findall('.//item')[0].findtext('description')
     assert 'RSS body description set from watch level at notification body - http://' in item_description
+    delete_all_watches(client)
