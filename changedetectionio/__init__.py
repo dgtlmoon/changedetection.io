98f884bbff0a16032d81fc4361a9a1655bcf645a
dgtlmoon <dgtlmoon@gmail.com>
2026-02-23 08:11:55 +0100

    0.53.7

--- changedetectionio/__init__.py
+++ changedetectionio/__init__.py
@@ -2,7 +2,7 @@
 
 # Read more https://github.com/dgtlmoon/changedetection.io/wiki
 # Semver means never use .01, or 00. Should be .1.
-__version__ = '0.53.6'
+__version__ = '0.53.7'
 
 from changedetectionio.strtobool import strtobool
 from json.decoder import JSONDecodeError
