f082c8ecc8a8fded44a4f77921fd14813b256130
skkzsh <skkzsh12@gmail.com>
2026-05-01 11:14:54 +0900

    Remove universal terms from translation scope

--- changedetectionio/forms.py
+++ changedetectionio/forms.py
@@ -728,7 +728,7 @@ class ValidateStartsWithRegex(object):
                 raise ValidationError(self.message or _l("Invalid value."))
 
 class quickWatchForm(Form):
-    url = StringField(_l('URL'), validators=[validateURL()])
+    url = StringField('URL', validators=[validateURL()])
     tags = StringTagUUID(_l('Group tag'), validators=[validators.Optional()])
     watch_submit_button = SubmitField(_l('Watch'), render_kw={"class": "pure-button pure-button-primary"})
     processor = RadioField(_l('Processor'), choices=lambda: processors.available_processors(), default=processors.get_default_processor)
