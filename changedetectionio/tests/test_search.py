e3d512f85735e845838672e922ef38ae2693c005
ponstream24 <87808547+ponstream24@users.noreply.github.com>
2026-09-11 16:11:15 +0900

    UI - Search - Fix search modal navigating to the host root on sub-path deployments
    base_path was referenced by search-modal.js but never defined, so searching
    always jumped to the host root instead of the X-Forwarded-Prefix sub-path.

--- changedetectionio/tests/test_search.py
+++ changedetectionio/tests/test_search.py
@@ -71,3 +71,12 @@ def test_search_in_tag_limit(client, live_server, measure_memory_usage, datastor
     assert urls[0].split(' ')[0].encode('utf-8') in res.data, urls[0].encode('utf-8')
     assert urls[1].split(' ')[0].encode('utf-8') not in res.data, urls[0].encode('utf-8')
 
+
+def test_search_modal_base_path(client, live_server, measure_memory_usage, datastore_path):
+    # search-modal.js navigates to base_path + '/?q=...', so base_path has to carry the
+    # reverse-proxy sub-path (SCRIPT_NAME), otherwise search jumps to the host root.
+    res = client.get(url_for("watchlist.index"))
+    assert b'const base_path = "";' in res.data
+
+    res = client.get("/", base_url="http://localhost/sub-path")
+    assert b'const base_path = "/sub-path";' in res.data
