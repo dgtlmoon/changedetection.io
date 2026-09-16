a431a0456e1ead06a4b9df64ba12227edb3500c4
dgtlmoon <dgtlmoon@gmail.com>
2026-09-16 18:12:59 +0200

    Option for simplifying the form

--- changedetectionio/blueprint/add_watch_ui/__init__.py
+++ changedetectionio/blueprint/add_watch_ui/__init__.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Blueprint, render_template, request, jsonify, make_response, flash, redirect, url_for
 from flask_babel import gettext
 from loguru import logger
 
-from changedetectionio import forms
+from changedetectionio import forms, strtobool
 from changedetectionio.auth_decorator import login_optionally_required
 from . import browser_config
 from changedetectionio.store import ChangeDetectionStore
@@ -34,6 +36,7 @@ def construct_blueprint(datastore: ChangeDetectionStore):
 
         return render_template(
             "add-watch-ui.html",
+            browser_backend_disabled=strtobool(os.getenv('ADD_WATCH_UI_BROWSER_BACKEND_DISABLED', 'False')),
             form=form,
             llm_configured=llm_configured,
             llm_intent_watch_placeholder=LLM_INTENT_WATCH_PLACEHOLDER,
@@ -78,7 +81,7 @@ def construct_blueprint(datastore: ChangeDetectionStore):
         url = (request.form.get('url') or '').strip()
         if not url.lower().startswith('http'):
             url='https://' + url
-            
+
         ok, reason = is_fetch_url_allowed(url)
         if not ok:
             logger.warning(f"Add-watch snapshot: refused '{url}' - {reason}")
